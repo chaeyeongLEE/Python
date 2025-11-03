51d2420ae062b7b0c4cecc5fc824ee5d3932cb7a
chaeyeongLEE <dh503401@naver.com>
2025-11-03 14:14:16 +0900

    파이썬 for문, def 문법 추가

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+print("[BOOT] app.py loaded")
 print('안녕'*10)
 print(123+456)
 
@@ -30,3 +31,30 @@ if 'K5' in 중고차 : print(message + '캬캬')
 if 'brand' in 중고차2 : print(message + '있다있어~~')
 if 'brand' in 중고차2 : print(중고차2['brand'])
 if 'benz' in 중고차2.values(): print("benz 있다있어~ 🚗💨")
+
+# for 반복문
+# for i in 반복할범위 : 반복해줄코드
+for i in range(0,3) : print("당근")
+
+중고차들 = ['K5', 'BMW', 'Tico']
+for i in 중고차들 : print(i)
+
+중고차들2 = [10, 20, 30]
+for i in 중고차들2 : print(i+1)
+
+# for n in range(1, 10) : print("짝수:",n*2)
+
+for dan in range(2,5) :
+    for num in range(1,10) :
+        print(dan*num)
+
+
+def 인사하기() : print('안녕하세요 중고차신뢰딜러 차은우입니다')
+인사하기()
+
+def 모자(숫자) : print(숫자 + 2)
+모자(2)
+
+def 수학연산(x) : return x + 2
+결과 = 수학연산(4)
+print(결과)
